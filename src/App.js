0ef77f2731397745f46dbcd608d2d216e0625509
dnjwm8612 <dnjwm8612@gmail.com>
2022-06-08 16:10:42 +0900

    1week event_useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import { useState } from 'react';
 
-function Header(){
-  return <header><h1><a href="/">Web</a></h1></header>
+function Header(props){
+  return <header><h1><a href="/" onClick={(evt) => {
+    console.log(props);
+    alert('evt', evt);
+    evt.preventDefault();
+    props.onSelect();
+  }}>Web</a></h1></header>
 }
 
 function Nav(props){
 const tagList = props.data.map((e) => {
-  return <li key={e.id}><a href={"/read/"+e.id}>{e.title}</a><br/>{e.body}</li>
+  return <li key={e.id}><a href={"/read/"+e.id} onClick={(evt) => {
+    evt.preventDefault();
+    props.onSelect(e.id);
+  }}>{e.title}</a></li>
 });
 // [
 //   <li>
@@ -32,16 +42,43 @@ function Article(props){
     </article>
 }
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+  console.log(mode, id);
   const topics = [
     {id: 1, title: 'html', body: 'html is ...'},
     {id: 2, title: 'css', body: 'css is ...'},
-  ]
+  ];
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
+  }else if(mode === 'READ'){
+    const topic = topics.filter(e => {
+      if(e.id === id){
+        return true;
+      }else{
+        return false;
+      }
+    })[0];
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  }
   return (
     <div>
-      <Header></Header>
-      <Nav data={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB!"></Article>
-      <Article title="HTML" body="Hello, HTML!"></Article>
+      <Header onSelect={() => {
+        setMode('WELCOME');
+      }}></Header>
+      <Nav data={topics} onSelect={(id) => {
+        setMode('READ');
+        setId(id);
+      }}></Nav>
+      {content}
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button variant="outlined" onClick={() => {
+          alert('create!');
+        }}>Create</Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
