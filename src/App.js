1236aad610e2ccfd1c809a13d45b1e6eaa519c72
dnjwm8612 <dnjwm8612@gmail.com>
2022-06-13 15:25:37 +0900

    week2 styled_evnet

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { useState } from 'react';
+import styled from 'styled-components';
 
 function Header(props){
-  return <header><h1><a href="/" onClick={(evt) => {
-    console.log(props);
-    alert('evt', evt);
-    evt.preventDefault();
+  return <header className={props.className}><h1><a href="/" onClick={(evt)=>{    
+    evt.preventDefault(); 
     props.onSelect();
-  }}>Web</a></h1></header>
+  }}>WWW</a></h1></header>
 }
+
+const HeaderStyled = styled(Header)`
+    border-bottom:1px solid gray;
+    color:red;
+`;
+
 
 function Nav(props){
 const tagList = props.data.map((e) => {
@@ -41,14 +45,32 @@ function Article(props){
     {props.body}
     </article>
 }
+
+function Create(props){
+  return(
+    <article><h2>Create</h2>
+      <form onSubmit={(evt) => {
+        evt.preventDefault();
+        const title = evt.target.title.value;
+        const body = evt.target.body.value;
+        props.onCreate(title, body);
+      }}>
+        <p><input type="text" name="title" placeholder='title'></input></p>
+        <p><textarea name="body" placeholder='body'></textarea></p>
+        <p><input type="submit" value="Create"></input></p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
   const [mode, setMode] = useState('WELCOME');
   const [id, setId] = useState(null);
-  console.log(mode, id);
-  const topics = [
+  const [nextId, setNextId] = useState(3);
+  const [topics, setTopics] = useState([
     {id: 1, title: 'html', body: 'html is ...'},
     {id: 2, title: 'css', body: 'css is ...'},
-  ];
+  ]);
   let content = null;
   if(mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB!"></Article>;
@@ -61,12 +83,25 @@ function App() {
       }
     })[0];
     content = <Article title={topic.title} body={topic.body}></Article>;
+  }else if(mode === 'CREATE'){
+    content = <Create onCreate={(title, body) => {
+      const newTopic = {id: nextId, title, body}
+      setTopics((current) => {
+        const newTopics = [...current];
+        newTopics.push(newTopic);
+        return newTopics;
+      });
+      setId(nextId);
+      setNextId((current) => {
+        return current + 1
+      });
+    }}></Create>
   }
   return (
     <div>
-      <Header onSelect={() => {
+      <HeaderStyled onSelect={() => {
         setMode('WELCOME');
-      }}></Header>
+      }}></HeaderStyled>
       <Nav data={topics} onSelect={(id) => {
         setMode('READ');
         setId(id);
@@ -74,11 +109,23 @@ function App() {
       {content}
       <ButtonGroup variant="contained" aria-label="outlined primary button group">
         <Button variant="outlined" onClick={() => {
-          alert('create!');
+          setMode("CREATE");
         }}>Create</Button>
         <Button variant="outlined">Update</Button>
       </ButtonGroup>
-      <Button variant="outlined">Delete</Button>
+      <Button variant="outlined" onClick={(evt) => {
+        evt.preventDefault();
+        setTopics((current) => {
+          const newTopics = [...current];
+          newTopics.splice(id, 1);
+          return newTopics;
+        })
+        setNextId((current) => {
+          return current - 1;
+        })
+        setId(nextId);
+        setMode('WELCOME');
+      }}>Delete</Button>
     </div>
   );
 }
